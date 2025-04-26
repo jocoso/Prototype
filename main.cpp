4576b1a39e70acfd490e14acbf4189a2efb0c6da
Josh <11726211+jocoso@users.noreply.github.com>
2025-04-26 13:54:06 +0000

    Upgrade
    Moved the for loop to its own function.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 #include <vector>
 
 
-//
+
+
+
 /**
 	After receiving a specified width and height, Board class generates a board.
 	input:
@@ -12,30 +14,33 @@
 */
 class Board {
 public:
-	Board(int width, int height) : _width(width), _height(height), _area(width * height), _board(width * height, '-') {}
-	// 
+	Board(int width, int height) : _width_ci(width), _height_ci(height), _area_ci(width * height), _board_lstch(width * height, '-') {}
+	
+	//
+	// Prints a vector function
+	void print_board() {
+		int count_i = 0;
+		
+		for (_it = _board_lstch.begin(); _it != _board_lstch.end(); ++_it) {
+			std::cout << *_it;
+			++count_i;
+			
+			// Turns array square-shaped.
+			if (count_i >= _width_ci) {std::cout << std::endl; count_i = 0;};
+		}
+	}
+	//
 	
 	// Displaying the board...
 	void display() {
-		
-		int count = 0;
-		
-		for (_it = _board.begin(); _it != _board.end(); ++_it) {
-			std::cout << *_it;
-			++count;
-			
-			// Turns array square-shaped.
-			if (count >= _width) {std::cout << std::endl; count = 0;};
-		}
+		print_board();
 	}
 	
-	//
 private:
-	const int _width, _height, _area;
-	std::vector<char> _board;
-	// 
+	const int _width_ci, _height_ci, _area_ci;
+	std::vector<char> _board_lstch;
 	std::vector<char>::iterator _it; // Local iterator.
-	//
+
 	
 };
 
