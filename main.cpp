291b71dea7f58079c1886be533df656fdeb00026
Josh <11726211+jocoso@users.noreply.github.com>
2025-04-26 13:44:00 +0000

    Upgrade
    Upgraded board to display squared array of characters.

--- main.cpp
+++ main.cpp
@@ -13,12 +13,34 @@
 class Board {
 public:
 	Board(int width, int height) : _width(width), _height(height), _area(width * height), _board(width * height, '-') {}
+	// 
+	
+	// Displaying the board...
+	void display() {
+		
+		int count = 0;
+		
+		for (_it = _board.begin(); _it != _board.end(); ++_it) {
+			std::cout << *_it;
+			++count;
+			
+			// Turns array square-shaped.
+			if (count >= _width) {std::cout << std::endl; count = 0;};
+		}
+	}
+	
+	//
 private:
 	const int _width, _height, _area;
 	std::vector<char> _board;
+	// 
+	std::vector<char>::iterator _it; // Local iterator.
+	//
+	
 };
-//
+
 
 int main() {
-	Board(8, 8);
+	Board(8, 8).display();
+	
 }
